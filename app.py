d9d26743aaf80c665638a1db148250999b5ecfcd
DaniBMelo <36732927+DaniBMelo@users.noreply.github.com>
2025-04-15 15:32:14 -0300

    from langchain.vectorstores import FAISS

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader, UnstructuredWordDocumentLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 import os
