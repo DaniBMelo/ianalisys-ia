6f1a5db89444cd280d723e496643c4660b25f544
DaniBMelo <36732927+DaniBMelo@users.noreply.github.com>
2025-04-15 12:53:20 -0300

    Update app.py
    from langchain.vectorstores import Chroma

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader, UnstructuredWordDocumentLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
